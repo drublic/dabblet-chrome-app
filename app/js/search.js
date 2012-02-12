ffe2988d7be396039e89072ba60e1df900b101e2
drublic <hans@sa-designz.de>
2012-02-12 23:52:20 +0100

    Search module
    Signed-off-by: drublic <hans@sa-designz.de>

---
+++ app/js/search.js
@@ -0,0 +1,19 @@
+define(['jquery', 'helpers/jquery.quicksearch'], function($) {
+  $('#search').quicksearch('table tbody tr', {
+
+    // Bind events
+    'bind' : 'keydown keyup click',
+
+    // Show files
+    'show': function() {
+      $(this).removeClass('hidden');
+    },
+    
+    // Hide files
+    'hide': function() {
+      $(this).addClass('hidden');
+    },
+    
+    'noResults' : 'tfoot'
+  });
+});
