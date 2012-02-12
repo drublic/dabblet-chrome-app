aac11c890be7e257dec915c13c7f1e83cfe58426
drublic <hans@sa-designz.de>
2012-02-12 23:52:13 +0100

    App module
    Signed-off-by: drublic <hans@sa-designz.de>

---
+++ app/js/load-app.js
@@ -0,0 +1,10 @@
+define(['jquery', 'plugins/text!tmpl/app.html', 'helpers/hogan'], function($, tmpl, Hogan) {
+  $('html').addClass('dabblet-app');
+
+  var template = Hogan.compile(tmpl).render($.parseJSON(localStorage.getItem('github-user-data')));
+
+  $('#container').html(template);
+  $('header').addClass('fade');
+
+  require(['dabblets']);
+});
